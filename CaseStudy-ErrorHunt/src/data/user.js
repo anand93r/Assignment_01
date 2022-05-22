3bdf031e8c27cbc264afa8f1a083a4b260216b23
anand93r <anand2007r@gmail.com>
2022-05-22 21:59:31 +0530

    error

---
+++ CaseStudy-ErrorHunt/src/data/user.js
@@ -0,0 +1,20 @@
+//changed users to user
+var user=[{
+    "id":1,
+    "uid":"aiswaryawalter@gmail.com",
+    "pwd":"aiswarya"
+},
+{
+    "id":2,
+    "uid":"surya@gmail.com",
+    "pwd":"surya"
+},
+{
+    "id":3,
+    "uid":"bazith@gmail.com",
+    "pwd":"bazith"
+}
+]
+
+
+module.exports=user;
